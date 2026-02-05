675d16d71cd7ff17e4eb298c72ec650d956bf90f
zachschool1 <zacharyjordan@my.whccd.edu>
2026-02-04 19:16:30 -0800

    step 4 complete

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@
             }
             
 
+            
+            int attempt = 1;
+            //this works at least once because thats what a do while loop does. do x. and then try and manipulate stuff IF it meets the requirements to be manipulated. like imagine in a game do health = 100 because you spawned in. while health is under 25, show that youre close to death, or weapon reload whatever it may be
+            do
+            {
+                Console.WriteLine($"Attempt {attempt}");
+            } while ( attempt <= 3);
 
         }
     }
