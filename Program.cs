dd6cf25761b6e563e6a240f8dc4ec43a11679414
zachschool1 <zacharyjordan@my.whccd.edu>
2026-02-04 19:27:41 -0800

    all done

--- Program.cs
+++ Program.cs
@@ -46,6 +46,17 @@
                 }
 
                 Console.WriteLine(number);
+
+
+                /*
+                 * I think the easiest loop for me to understand is for loops, since i think I have the most experience.
+                 * I do think While and doWhile loops are very useful, i just dont know many uses since i havent done it first hand yet.
+                 * 
+                 * 
+                 * I think the number that was the hardest to 'trace' would probably be the For loops, since you have to understand that you are doing 3 things at once. initialize a counter, say the parameter that it runs, and how to increment the counter all in one little ()
+                 * 
+                 *comment helpp because it helps to understand why a break or a continue has different things it does.
+                 */
             }
         }
     }
